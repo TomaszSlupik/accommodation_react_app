63fa038d02a3c8bca0f93224321c303f74cd1c86
TomaszSlupik <99093128+TomaszSlupik@users.noreply.github.com>
2022-10-03 07:47:24 +0200

    next

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
 			id: 1,
 			name: "Lukulus",
 			city: "Cypr",
-			description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate beatae facere, pariatur, maxime quod molestias itaque eligendi delectus optio dolore distinctio unde! Odit, ipsam? Iusto nam sapiente est facere velit.',
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate beatae facere, pariatur, maxime quod molestias itaque eligendi delectus optio dolore distinctio unde! Odit, ipsam? Iusto nam sapiente est facere velit.",
 			mark: 9.2,
 			image: "",
 		},
@@ -47,28 +48,41 @@ class App extends Component {
 			name: "Warszawski",
 			mark: 8.4,
 			city: "Grecja",
-		},
+		}
 	];
 	state = {
-		hotels: this.hotels,
+		hotels: [],
+		loading: true
 	};
+
 
 	searchHandler(term) {
 		console.log(`Szukaj z app`, term);
-		const hotels = [...this.hotels]
-		.filter((x) => x.name
-		.toLowerCase()
-		.includes(term.toLowerCase()));
+		const hotels = [...this.hotels].filter((x) =>
+			x.name.toLowerCase().includes(term.toLowerCase())
+		);
 		this.setState({ hotels });
 	}
 
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({ hotels: this.hotels });
+			this.setState({ loading: false });
+		}, 1000);
+		console.log(`Konstruktor zamontowany`);
+	}
+
 	render() {
+		console.log(`Komponent wyrenderowany`);
 		return (
 			<div className="App">
 				<Header onSearch={(term) => this.searchHandler(term)} />
 				<Menu />
-				<Hotels hotels={this.state.hotels} />
-				<Hotel />
+				{this.state.loading ? (
+					<p>Ładowanie danych...</p>
+				) : (
+					<Hotels hotels={this.state.hotels} />
+				)}
 			</div>
 		);
 	}
